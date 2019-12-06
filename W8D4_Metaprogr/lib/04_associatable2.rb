e2afcedb92aaebf90a66eabab566cfd938c6d903
Yenisbel Valle <yenisbelv@gmail.com>
2019-12-06 15:54:34 -0800

    Add active record metaprog v2

--- W8D4_Metaprogr/lib/04_associatable2.rb
+++ W8D4_Metaprogr/lib/04_associatable2.rb
@@ -6,5 +6,33 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     # ...
+
+    p name, through_name, source_name
+
+    define_method(name)do
+
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through = through_options.table_name
+      through_pri = through_options.primary_key
+      through_for = through_options.foreign_key
+
+      source = source_options.table_name
+      source_pri = source_options.primary_key
+      source_for = source_options.foreign_key
+
+      arg = self.send(through_for)
+
+      query = DBConnection.execute(<<-SQL, arg)
+        SELECT #{source}.*
+        FROM #{through}
+        JOIN #{source} ON #{through}.#{source_for} = #{source}.#{source_pri}
+        WHERE #{through}.#{through_pri} = ?
+      SQL
+
+      source_options.model_class.parse_all(query).first
+    end
   end
 end
+
